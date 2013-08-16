89804ca82a40e9491d3e0cc80ab6ddcf897c608f
Ulf Wiger <ulf@feuerlabs.com>
2013-08-16 11:03:46 +0200

    playing around with exometer_test

--- src/exometer_test.erl
+++ src/exometer_test.erl
@@ -4,8 +4,7 @@
 
 
 folsom_slide(W, Int) ->
-    application:stop(folsom),
-    application:start(folsom),
+    reset(folsom),
     ok = folsom_metrics:new_histogram(Name = folsom_slide, slide, W),
     try
 	update_hist(_N = W * 10 * 5, Name, _Sleep = 10, _ReadInt = Int,  Int, [])
@@ -13,15 +12,41 @@ folsom_slide(W, Int) ->
 	catch folsom_metrics:delete_metric(Name)
     end.
 
+folsom_update(W, Ps, N) ->
+    reset(folsom),
+    ok = folsom_metrics:new_histogram(Name = folsom_slide, slide, W),
+    try  {T,_} = timer:tc(?MODULE, folsom_update_p, [Name, Ps, N]),
+	 T
+    after
+	catch folsom_metrics:delete_metric(Name)
+    end.
+
+reset(App) ->
+    application:stop(App),
+    application:start(App).
+
 my_slide(W, Int) ->
-    application:stop(exometer),
-    application:start(exometer),
+    my_slide(W, Int, full).
+
+my_slide(W, Int, Hist) when Hist==full; Hist==mini ->
+    reset(exometer),
     true = new_h(Name = my_slide, W),
     try
-	update_h(_N = W * 10 * 5, Name, _Sleep = 10, _ReadInt = Int,  Int, [])
+	update_h(_N = W * 10 * 5, Name, _Sleep = 10, _ReadInt = Int,  Int,
+		 Hist, [])
     after
 	unlink(whereis(Name)),
-	exit(whereis(Name), foo)
+	exit(whereis(Name), kill)
+    end.
+
+my_slide_update(W, Ps, N) ->
+    reset(exometer),
+    true = new_h(Name = my_slide, W),
+    try {T, _} = timer:tc(?MODULE, my_slide_update_p, [Name, Ps, N]),
+        T
+    after
+	unlink(whereis(Name)),
+	exit(whereis(Name), kill)
     end.
 
 
@@ -44,6 +69,28 @@ update_hist(_, Name, _, _, _, Acc) ->
     [trim_(bear:get_statistics(Acc)),
      trim(timer:tc(folsom_metrics, get_histogram_statistics, [Name]))].
 
+folsom_update_p(Name, Ps, N) ->
+    pmap(Ps, fun() ->
+		     folsom_update_p_(N, Name)
+	     end).
+
+my_slide_update_p(Name, Ps, N) ->
+    pmap(Ps, fun() ->
+		     my_slide_update_p_(N, Name)
+	     end).
+
+folsom_update_p_(N, Name) when N > 0 ->
+    ok = folsom_metrics:notify(Name, 1),
+    folsom_update_p_(N-1, Name);
+folsom_update_p_(_, _) ->
+    ok.
+
+my_slide_update_p_(N, Name) when N > 0 ->
+    true = update(Name, exometer:timestamp(), 1, N rem 3 == 0),
+    my_slide_update_p_(N-1, Name);
+my_slide_update_p_(_, _) ->
+    ok.
+
 
 new_h(Name, W) ->
     Me = self(),
@@ -61,29 +108,30 @@ new_h(Name, W) ->
 	    error(timeout)
     end.
 
-update_h(N, Name, Sleep, ReadInt, ReadInt0, Acc) when N > 0 ->
-    {T,true} = timer:tc(?MODULE,update,[Name, 1, true]),
+update_h(N, Name, Sleep, ReadInt, ReadInt0, Hist, Acc) when N > 0 ->
+    {T,true} = timer:tc(?MODULE,update,[Name, N div 7, true]),
     Acc1 = [T|Acc],
     {R, ReadInt1, Acc2} =
 	if ReadInt =< 0 ->
 		{[trim_(bear:get_statistics(Acc1)),
-		  trim(timer:tc(?MODULE,get_histogram_statistics, [Name]))],
+		  trim(timer:tc(?MODULE,get_histogram_statistics,
+				[Name, Hist]))],
 		 ReadInt0, []};
 	   true ->
 		{[], ReadInt - 1, Acc1}
 	end,
     timer:sleep(Sleep),
-    R ++ update_h(N-1, Name, Sleep, ReadInt1, ReadInt0, Acc2);
-update_h(_, Name, _, _, _, Acc) ->
+    R ++ update_h(N-1, Name, Sleep, ReadInt1, ReadInt0, Hist, Acc2);
+update_h(_, Name, _, _, _, Hist, Acc) ->
     [trim_(bear:get_statistics(Acc)),
-     trim(timer:tc(?MODULE, get_histogram_statistics, [Name]))].
+     trim(timer:tc(?MODULE, get_histogram_statistics, [Name, Hist]))].
 
 trim({T,H}) ->
     {T, trim_(H)}.
 
 trim_(Hist) ->
     [{K,V} || {K,V} <- Hist,
-	      lists:member(K, [min,max,median])].
+	      lists:member(K, [min,max,median,percentile])].
 
 update(N, Value, true) ->
     call(N, {update, Value});
@@ -91,13 +139,27 @@ update(N, Value, false) ->
     N ! {async_update, Value},
     true.
 
+update(N, TS, Value, true) ->
+    call(N, {update, TS, Value});
+update(N, TS, Value, false) ->
+    N ! {async_update, TS, Value},
+    true.
 
 get_values(N) ->
     call(N, get_values).
 
+get_histogram_statistics(N, full) ->
+    get_histogram_statistics(N);
+get_histogram_statistics(N, mini) ->
+    get_mini_histogram(N).
+
 get_histogram_statistics(N) ->
     Values = get_values(N),
     bear:get_statistics(Values).
+
+get_mini_histogram(N) ->
+    Values = get_values(N),
+    mini_hist(Values).
 
 call(N, Req) ->
     N ! {self(), Req},
@@ -110,6 +172,13 @@ call(N, Req) ->
 
 h_loop(N, S) ->
     receive
+	{From, {update, TS, Value}} ->
+	    S1 = exometer_slide:add_element(TS, Value, S),
+	    From ! {N, true},
+	    h_loop(N, S1);
+	{async_update, TS, Value} ->
+	    S1 = exometer_slide:add_element(TS, Value, S),
+	    h_loop(N, S1);
 	{From, {update, Value}} ->
 	    S1 = exometer_slide:add_element(Value, S),
 	    From ! {N, true},
@@ -122,3 +191,60 @@ h_loop(N, S) ->
 	    From ! {N, Res},
 	    h_loop(N, S)
     end.
+
+pmap(N, F) ->
+    Pids = [spawn_monitor(fun() ->
+				  exit({ok, F()})
+			  end) || _ <- lists:seq(1,N)],
+    collect(Pids).
+
+collect([{Pid,Ref}|T]) ->
+    receive
+	{'DOWN', Ref, _, _, R} ->
+	    case R of
+		{ok, Res} ->
+		    [Res | collect(T)];
+		Other ->
+		    [exit(P, kill) || {P,_} <- T],
+		    error(Other)
+	    end
+    after 60000 ->
+	    [exit(P, kill) || {P,_} <- [{Pid,Ref}|T]],
+	    error(timeout)
+    end;
+collect([]) ->
+    [].
+
+collect_inout_trace() ->
+    receive
+	{trace_ts,P,in,_,TS} ->
+	    receive
+		{trace_ts,P,out,_,TS2} ->
+		    [timer:now_diff(TS2, TS)
+		     | collect_inout_trace()]
+	    after 10000 ->
+		    error(timeout)
+	    end
+    after 0 ->
+	    []
+    end.
+
+mini_hist(Vals) ->
+    Sorted = lists:sort(Vals),
+    L = length(Sorted),
+    Items = [{min,1}, {50, perc(0.5,L)}, {75, perc(0.75,L)}, {90, perc(0.9,L)},
+	     {95, perc(0.95,L)}, {99, perc(0.99,L)}, {max,L}],
+    [Min|Rest] = pick_items(Sorted, 1, Items),
+    [Min, {percentile, lists:keydelete(max,1,Rest)}, lists:last(Rest)].
+
+pick_items([H|T], P, [{Tag,P}|Ps]) ->
+    [{Tag,H} | pick_items(T, P+1, Ps)];
+pick_items([_|T], P, Ps) ->
+    pick_items(T, P+1, Ps);
+pick_items([], _, Ps) ->
+    [{Tag,undefined} || {Tag,_} <- Ps].
+
+
+
+perc(P, Len) ->
+    round(P * Len).
