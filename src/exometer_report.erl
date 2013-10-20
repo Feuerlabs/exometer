d999f1e9c73055b403e2da586fb522bb62fa9ab3
Magnus Feuer <magnus@feuerlabs.com>
2013-10-20 16:57:13 -0700

    Exometer report now is a generic subscriber manager that can deliver both to processes and modules.

--- src/exometer_report.erl
+++ src/exometer_report.erl
@@ -11,25 +11,28 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+-export([start_link/0,
+	 subscribe/4]).
+
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE). 
--type probe() :: atom().
+-type probe() :: list().
 -type datapoint() :: atom().
--type argument() :: any().
 -type value() :: any().
+-type mod_state() :: any().
 -type recipient() :: pid() | atom().
+-type options() :: [ { atom(), any()} ].
 
 -type key() :: { pid | module, recipient(), probe(), datapoint()}.
 
 %% Callback for function, not cast-based, reports that
 %% are invoked in-process.
--callback exometer_report(probe(), datapoint(), value(), argument()) -> any().
-
+-callback report(probe(), datapoint(), value(), mod_state()) -> any().
+-callback init(options()) -> any().
 
 -record(subscriber, { 
 	  key :: key(),
@@ -37,8 +40,14 @@
 	 }).
 
 
+-record(mod_state, {
+	  module :: atom(),
+	  state :: any()
+	 }).
+
 -record(st, { 
-	  subscribers:: [ #subscriber{} ]
+	  subscribers:: [ #subscriber{} ],
+	  mod_states:: [ #mod_state{} ]
 	 }).
 
 %%%===================================================================
@@ -53,8 +62,17 @@
 %% @end
 %%--------------------------------------------------------------------
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    {ok, Opts} = application:get_env(exometer, exometer_report),
+    gen_server:start_link({local, ?SERVER}, ?MODULE,  Opts, []).
 
+
+subscribe(Recipient, Probe, DataPoint, Interval) when is_pid(Recipient) ->
+    gen_server:call(?MODULE, { subscribe, pid, Recipient, Probe, DataPoint, Interval });
+
+subscribe(Recipient, Probe, DataPoint, Interval) when is_atom(Recipient) ->
+    gen_server:call(?MODULE, { subscribe, module, Recipient, Probe, DataPoint, Interval }).
+
+	
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
@@ -70,9 +88,27 @@ start_link() ->
 %%                     {stop, Reason}
 %% @end
 %%--------------------------------------------------------------------
-init([]) ->
+init(Opts) ->
+
+    %% Dig out the mod opts.
+    %% { modules, [ {module1, [{opt1, val}, ...]}, {module2, [...]}]}
+    {value, {modules, Modules}, _Opts1 } = lists:keytake(modules, 1, Opts), 
+    
+    %% Traverse list and init modules.
+    %% If init fails, leave module out of module state list
+    ModStates = lists:foldl(fun(ModConfig, Acc) ->  
+			      { Mod, ModOpts} = ModConfig,
+			      case Mod:init(ModOpts) of 
+				  {ok, St} ->[ { Mod, St } | Acc ];
+				  _ -> Acc
+			      end
+		       end, [], Modules),
+    
+    
+
     {ok, #st{ 
-      subscribers = []
+       subscribers = [],
+       mod_states = ModStates
       }}.
 
 %%--------------------------------------------------------------------
@@ -89,10 +125,13 @@ init([]) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_call({subscribe, Type, Recipient, Arg, Probe, DataPoint, Interval }, _From, St) ->
+handle_call({subscribe, Type, Recipient, Probe, DataPoint, Interval }, _From, St) ->
+
     %% FIXME: Validate Probe and datapoint
+    %% FIXME: Monitor on pids.
+
     {ok, TRef } = timer:send_after(Interval, self(), 
-				   { report, Type, Recipient, Arg, Probe, DataPoint, Interval }),
+				   { report, Type, Recipient, Probe, DataPoint, Interval }),
 
     {reply, ok, St#st { subscribers = [ #subscriber {
 					   key = { Type, Recipient, Probe, DataPoint},
@@ -111,6 +150,7 @@ handle_call({unsubscribe, Type, Recipient, Probe, DataPoint }, _From, St) ->
 
 
 handle_call(_Request, _From, State) ->
+    io:format("exometer_report:handle_call(??): ~p~n", [ _Request ]),
     Reply = ok,
     {reply, Reply, State}.
 
@@ -137,34 +177,58 @@ handle_cast(_Msg, State) ->
 %%                                   {stop, Reason, State}
 %% @endo
 %%--------------------------------------------------------------------
-handle_info({ report, Type, Recipient, Arg, Probe, DataPoint, Interval }, St) ->
+handle_info({ report, Type, Recipient, Probe, DataPoint, Interval }, St) ->
     case exometer_entry:get_value(Probe, [ DataPoint ]) of
-	{ ok, Val } ->
-	    %% Distribute probe value to pid subscriber, or module.
-	    if Type =:= pid ->
-		    Recipient ! { exometer_report, Probe, DataPoint, Val, Arg };
-	       true ->
-		    Recipient:exometer_update(Probe, DataPoint, Val, Arg)
+	{ ok, [ { _, Val}] } ->
+	    %% Distribute probe value to pid subscriber or module, depending on type.
+	    %% Store indication if we should re-arm the timer, and the new module states
+	    %% (for module reporting).
+	    { ReArmTimer, NewModStates } = 
+		if Type =:= pid ->
+			%% Send a message to the recipient process
+			%% FIXME: Monitor or exception handling for dead pids
+			Recipient ! { exometer_report, Probe, DataPoint, Val },
+			{ true, St#st.mod_states };
+
+		   true ->
+			%% Invoke the module with probe, datapoint and current module state
+			%% New state will be saved.
+			{ value, { _, ModSt}, TmpModList } = lists:keytake(Recipient, 1, St#st.mod_states) ,
+
+			%% Check that the reporting went well. If not, remove from Mod State list
+			case  Recipient:report(Probe, DataPoint, Val, ModSt) of
+			    { ok, NewModSt } -> { true, [ { Recipient, NewModSt } | TmpModList ]};
+			    _  -> { false, TmpModList }
+			end
+		end,
+
+	    %% If the reporting went well, re-arm the timer for next round
+	    TRef = if ReArmTimer =:= true ->
+		    {ok, T } = timer:send_after(Interval, self(), 
+						   { report, Type, Recipient, Probe, DataPoint, Interval }),
+			   T;
+		    true -> undefined
 	    end,
 
-	    %% Re-arm probe.
-	    {ok, TRef } = timer:send_after(Interval, self(), 
-					   { report, Type, Recipient, Probe, DataPoint, Interval }),
-
 	    %% Replace the pid_subscriber info with a record having the new timer ref.
-	    {noreply, St#st { subscribers = 
+	    %% Replace mod states with the updates state returned by Recipient:report()
+	    {noreply, St#st { mod_states = NewModStates,
+			      subscribers = 
 			     lists:keyreplace({Type, Recipient, Probe, DataPoint}, 
+					      #st.subscribers,
 					      St#st.subscribers,
 					      #subscriber {
-						      key = {Type, Recipient, Probe, DataPoint},
+						      key = { Type, Recipient, Probe, DataPoint},
 						      t_ref = TRef
 						     })}};
 	%% Entry removed while timer in progress.
 	_ ->
+	    io:format("Probe(~p) Datapoint(~p) not found~n", [ Probe, DataPoint]),
 	    {noreply, St }
     end;
 
 handle_info(_Info, State) ->
+    io:format("exometer_report:info(??): ~p~n", [ _Info ]),
     {noreply, State}.
 
 %%--------------------------------------------------------------------
