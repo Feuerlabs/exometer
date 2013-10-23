ac11f5e32a14056acb35413708d527adcf60dc9c
Magnus Feuer <magnus@feuerlabs.com>
2013-10-23 15:59:39 -0700

    Added examples that updates configured static subscription  from priv/app.config

--- test.erl
+++ test.erl
@@ -3,9 +3,15 @@
 
 application:start(exometer).
 exometer_admin:preset_defaults().
-exometer_entry:new("test", counter).
+exometer_entry:new("test1", counter).
+exometer_entry:new("test2", counter).
+
 exometer_report:start_link().
-exometer_report:subscribe(exometer_report_graphite, "test", counter, 1000).
+
+exometer_report:subscribe(exometer_report_graphite, "test1", counter, 1000).
+
+exometer_entry:update("test1", 1).
+exometer_entry:update("test2", 2).
 
 
-exometer_entry:update("test", 1).
+
